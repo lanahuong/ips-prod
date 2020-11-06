53dd58ad8674805145681e9ab38fc5a5c86aca16
Lana Scravaglieri <lana.scravaglieri@gmail.com>
2020-11-06 17:14:53 +0100

    Class Poly is a refactored copy of Hermit for the new project

---
+++ src/Poly.cpp
@@ -0,0 +1,20 @@
+#include "Poly.h"
+
+void Poly::calcHermite(uint nMax, const arma::rowvec &zRowvec) {
+  /**
+   * If the parameters are nonsense the matrix (0) is returned
+   */
+  arma::uword rowLen = zRowvec.size();
+  hermitePolynomial = arma::mat(nMax + 1, rowLen, arma::fill::zeros);
+  hermitePolynomial.row(0) = arma::vec(rowLen, arma::fill::ones).t();
+  if (likely(nMax > 0)) {
+    hermitePolynomial.row(1) = 2 * zRowvec;
+    for (arma::uword i = 2; i <= nMax; i++) {
+      hermitePolynomial.row(i) =
+          hermitePolynomial.row(1) % hermitePolynomial.row(i - 1) -
+          2. * ((double)i - 1.) * hermitePolynomial.row(i - 2);
+    }
+  }
+}
+
+arma::vec Poly::hermite(int n) { return hermitePolynomial.row(n); }
---
+++ src/Poly.h
@@ -0,0 +1,48 @@
+/**
+ * @file Poly.h
+ * This file implements a function to compute all the polynomials on a
+ * vector up to a fixed order
+ */
+
+#ifndef POLY_H
+#define POLY_H
+
+/**
+ * Tells the branch predictor that it is very likely that the condition is true
+ * Wether it does something or not is implementation dependant.
+ */
+#define likely(x) __builtin_expect((x), 1)
+
+/**
+ * Tells the branch predictor that the condition is very unlikely false.
+ */
+#define unlikely(x) __builtin_expect((x), 0)
+
+#include <armadillo>
+#include <vector>
+
+/**
+ * @class Poly
+ * Used to compute Hermite and Laguerre polynomials
+ */
+class Poly {
+private:
+  arma::mat hermitePolynomial;
+
+public:
+  /**
+   * @brief Iteratively evaluate the Hermite polynomial on a vector
+   * @param nMax max degree to compute
+   * @param zRowvec input vector
+   * @return an matrix where each row corresponds to a degree
+   */
+  void calcHermite(uint nMax, const arma::rowvec &zRowvec);
+
+  /**
+   * @brief Get the Hermite polynomial previously computed of rank n-1
+   * @param n the rank + 1 of the polynomial
+   */
+  arma::vec hermite(int n);
+};
+
+#endif // POLY_H!
