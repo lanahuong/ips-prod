lanahuong/ips-prod

commit 8efed9d4f2286d4c30d8c529f51dc3016ac9ba26

We really need to remove this code from the main. I deleted it by mistake, here it is

src/main.cpp
@@ -1,26 +1,36 @@
 #include "main.h"
 #include "NuclearDensityCalculator.h"
 #include "savers/3Dsavers.h"
-#include "Chrono.hpp"
 
 using namespace std;
 
-
-int main()
-{
-
+int main() {
     NuclearDensityCalculator nuclearDensityCalculator;
     int r_bound = 10;
-    double r_step = 01;
-    int r_len = 2*r_bound/r_step+1;
+    int r_step = 1;
+    int r_len = 2 * r_bound / r_step + 1;
     int z_bound = 20;
-    double z_step = 02;
-    int z_len = 2*z_bound/z_step+1;
+    int z_step = 2;
+    int z_len = 2 * z_bound / z_step + 1;
 
 
-    //arma::mat res = nuclearDensityCalculator.naive_method(arma::regspace(-r_bound, r_step, r_bound), arma::regspace(-z_bound, z_step, z_bound));
-    Chrono c("main");
-    for (int i = 0; i<1000; i++) arma::mat res = nuclearDensityCalculator.optimized_method3(arma::regspace(-r_bound, r_step, r_bound), arma::regspace(-z_bound, z_step, z_bound));
+    arma::mat res = nuclearDensityCalculator.naive_method(arma::regspace(-r_bound, r_step, r_bound), arma::regspace(-z_bound, z_step, z_bound));
+    res = nuclearDensityCalculator.optimized_method1(arma::regspace(-r_bound, r_step, r_bound), arma::regspace(-z_bound, z_step, z_bound));
+    res = nuclearDensityCalculator.optimized_method2(arma::regspace(-r_bound, r_step, r_bound), arma::regspace(-z_bound, z_step, z_bound));
 
+
+    arma::cube cube = arma::zeros(r_len, r_len, z_len);
+    int r = -r_bound;
+    for (int k = 0; k < r_len; k++) {
+        for (int t = 0; t < 40; t++) {
+            double theta = PI * t / 20;
+            int x = (r * cos(theta) + r_bound);
+            int y = (r * sin(theta) + r_bound);
+            cube.tube(x, y) = res.row(k);
+        }
+        r++;
+    }
+
+    std::cout << cubeToDf3(cube);
     return 0;
 }

commit 372e9c2ec400c6e9c0feda7d486ba3041390a8fd

Complete comments and fix a bug introduced in commit 8efed9d4

src/Basis.h
@@ -51,10 +51,10 @@ public:
 
     /**
      * Compute the r part of the function
-     * @param rVec
-     * @param m Angular momentum ?
-     * @param n Principal quantum number
-     * @param use_mem Is set to true if the function is called from a memoized context
+     * @param rVec vector of r values
+     * @param m quantum number
+     * @param n quantum number
+     * @param use_mem is set to true if the function is called from a memoized context
      * Protects the user if the user calls the function directly while the class was
      * set to be memoised.
      * @return the r part of the function
@@ -63,13 +63,15 @@ public:
 
     /**
      * Function used to access memoised values.
+     * @param m quantum number
+     * @param n quantum number
      */
     arma::vec rPart_mem(int m, int n);
 
     /**
      * Compute the z part of the function
-     * @param zVec
-     * @param nz
+     * @param zVec vector of z values
+     * @param nz quantum number
      * @param use_mem Is set to true if the function is called from a memoized context
      * Protects the user if the user calls the function directly while the class was
      * set to be memoised.
@@ -79,26 +81,26 @@ public:
 
     /**
      * Function used to access memoised values.
-     * @param nz
+     * @param nz quantum number
      */
     arma::vec zPart_mem(int nz);
 
     /**
      * Computes
-     * @param m Quantum number
-     * @param n Quantum number
-     * @param nz Quantum number
-     * @param rVec Vector of r[i]
-     * @param zVec Vector of r[j]
+     * @param m quantum number
+     * @param n quantum number
+     * @param nz quantum number
+     * @param rVec vector of r[i]
+     * @param zVec vector of r[j]
      * @return a matrix where Mat(i,j) corresponds to \f$ \Psi_{m_a, n_a, n_{za}} (r_i, z_j) \f$
      */
     arma::mat basisFunc(int m, int n, int nz, const arma::vec& rVec, const arma::vec& zVec);
 
     /**
      * @see basisFunc but uses vectors that were provided in the constructor
-     * @param m Quantum number
-     * @param n Quantum number
-     * @param nz Quantum number
+     * @param m quantum number
+     * @param n quantum number
+     * @param nz quantum number
      * @return
      */
     arma::mat basisFunc_mem(int m, int n, int nz);
@@ -121,8 +123,8 @@ private:
      * Given the definition and nMax being >=0 , if Q is null the sup is not defined
      * if Q non null, nMax is equal to
      * \f$ \lfloor  (N+2)Q^{-1/3} -0.5 Q^{-1}  \rfloor  \f$
-     * @param N
-     * @param Q
+     * @param N truncation parameter
+     * @param Q truncation parameter
      */
     static int calcMMax(int N, double Q);
 
@@ -133,8 +135,8 @@ private:
 
     /**
      * Initializes the n_zmax matrix
-     * @param N
-     * @param Q
+     * @param N truncation parameter
+     * @param Q truncation parameter
      */
     arma::imat calcN_zMax(int N, double Q);
 

src/NuclearDensityCalculator.cpp
@@ -154,14 +154,6 @@ NuclearDensityCalculator::NuclearDensityCalculator()
 }
 
 /**
- *
- * @param m
- * @param n
- * @param n_z
- * @param mp
- * @param np
- * @param n_zp
- * @return
  */
 inline double NuclearDensityCalculator::rho(int m, int n, int n_z, int mp, int np, int n_zp) const
 {

src/NuclearDensityCalculator.h
@@ -13,21 +13,27 @@
  */
 class NuclearDensityCalculator {
 private:
-    const int N = 14;
-    const double Q = 1.3;
-    const double br = 1.935801664793151;
-    const double bz = 2.829683956491218;
-    arma::mat imported_rho_values;
-    Basis basis;
+    const int N = 14; /** truncation parameter */
+    const double Q = 1.3; /** truncation parameter */
+    const double br = 1.935801664793151; /** radius deformation factor */
+    const double bz = 2.829683956491218; /** z deformation factor */
+    arma::mat imported_rho_values; /** rho values from file */
+    Basis basis; /** basis of functions */
 
     /**
      * Computes the value of rho for the given
      * nuclear parameters and the basis used in the constructor
      * or hardcoded.
+     * @param n quantum number of a
+     * @param m quantum number of a
+     * @param n_z quantum number of a
+     * @param np quantum number of b
+     * @param mp quantum number of b
+     * @param n_zp quantum number of b
      *
      * @warning For now just returns hard coded values because
      * we dont know how to compute them.
-     * @return
+     * @return rho(n, m, n_z, np, mp, n_zp)
      */
     inline double rho(int m, int n, int n_z, int mp, int np, int n_zp) const;
 
@@ -51,14 +57,17 @@ public:
 
     /**
      * Naive method seen in the class
-     * @param rVals
-     * @param zVals
-     * @return a matrix
+     * @param rVals vector of r values (radius)
+     * @param zVals vector of z values
+     * @return a matrix of density values for rVals x zVals (cartesian products giving coordinates)
      */
     arma::mat naive_method(const arma::vec& rVals, const arma::vec& zVals) ;
 
     /**
      * Optimized method 1
+     * @param rVals vector of r values (radius)
+     * @param zVals vector of z values
+     * @return a matrix of density values for rVals x zVals (cartesian products giving coordinates)
      */
     arma::mat optimized_method1(const arma::vec& rVals, const arma::vec& zVals) ;
 
@@ -66,9 +75,9 @@ public:
      * Manually trying to factor out some values, but doesnt really work in a clean manner
      * Plus multithreading is a useless as the first loop has very unequal branches in term
      * of computing needed. Thus the threads are unbalanced
-     * @param rVals
-     * @param zVals
-     * @return
+     * @param rVals vector of r values (radius)
+     * @param zVals vector of z values
+     * @return a matrix of density values for rVals x zVals (cartesian products giving coordinates)
      */
     arma::mat optimized_method2(const arma::vec& rVals, const arma::vec& zVals);
 
@@ -78,9 +87,9 @@ public:
      * It uses multithreading with openMP but can be ported to use native threads
      * It uses the factorisation helper to extract the four sub_sums from the
      * naive one.
-     * @param rVals
-     * @param zVals
-     * @return
+     * @param rVals vector of r values (radius)
+     * @param zVals vector of z values
+     * @return a matrix of density values for rVals x zVals (cartesian products giving coordinates)
      */
     arma::mat optimized_method3(const arma::vec& rVals, const arma::vec& zVals) const;
 

src/Poly.cpp
@@ -1,7 +1,7 @@
 #include "Poly.h"
 
 void Poly::calcHermite(uint nMax, const arma::vec &vec) {
-    /**
+    /*
      * If the parameters are nonsense the matrix (0) is returned
      */
     arma::uword rowLen(vec.size());
@@ -26,7 +26,7 @@ arma::vec  Poly::laguerre(int m, int n)const {
 }
 
 void Poly::calcLaguerre(int mMax, int nMax, const arma::vec &z) {
-    /**
+    /*
      * Initialize the cube and helper vectors. Initialize also the first two slices (if needed) for the recursion
      */
     laguerrePolynomial = arma::cube(mMax, z.n_elem, nMax, arma::fill::ones);
@@ -37,7 +37,7 @@ void Poly::calcLaguerre(int mMax, int nMax, const arma::vec &z) {
         laguerrePolynomial.slice(1) = 1+(m*row_ones)-col_ones*z.as_row();
     }
 
-    /**
+    /*
      * Now we build our cube, each slice is a matrix and within such slice N is constant, so we
      * will operate on slices, its faster.
      * The slice 0 is already filled with ones

src/Poly.h
@@ -27,9 +27,10 @@
  */
 class Poly {
 private:
+  /** matrix of hermite polynomials (n,z) */
   arma::mat hermitePolynomial;
+  /** cube of laguerre polynomials (m,n,z)*/
   arma::cube laguerrePolynomial;
-
 public:
   /**
    * @brief Iteratively evaluate the Hermite polynomial on a vector

src/Saver.cpp
@@ -13,8 +13,6 @@ void Saver::saveToCSV(const arma::mat& d, std::string filename)
 
 /**
  *
- * @param m
- * @param filename
  */
 void Saver::cubeToDf3(const arma::cube &m, const std::string& filename) {
     std::stringstream ss(std::stringstream::out | std::stringstream::binary);

src/Saver.h
@@ -27,8 +27,8 @@ class Saver {
 
   /**
    *
-   * @param m
-   * @param filename
+   * @param m the cube to save to df3
+   * @param filename the name of the df3 file (must end with .df3)
    */
   static void cubeToDf3(const arma::cube &m, const std::string& filename);
 };

src/main.cpp
@@ -19,7 +19,7 @@ int main()
 
     Saver::saveToCSV(res, "tmp/density-r-z.csv");
     
-    arma::cube cube = nuclearDensityCalculator.density_cartesian(xyPoints, zPoints, rVals, res);
+    arma::cube cube = NuclearDensityCalculator::density_cartesian(xyPoints, zPoints, rVals, res);
 
     Saver::cubeToDf3(cube, "tmp/density-r-z.df3");
 